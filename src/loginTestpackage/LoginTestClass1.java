57c9c0346176674e38068635e9c6c8de59063a2a
RahulPerkiwar <rperkiwar2089@gmail.com>
2024-03-25 13:26:22 +0530

    Chnages done by user2

--- src/loginTestpackage/LoginTestClass1.java
+++ src/loginTestpackage/LoginTestClass1.java
@@ -7,8 +7,8 @@ public class LoginTestClass1
 	public void logiinclassmethod() {
 		
 		System.out.println("Enter the Valid credentials");
-		System.out.println("Enetr the Username");
-		System.out.println("Enter the password");
+		System.out.println("Enetr the Username by second user and also password enterd");
+	    System.out.println("Modified changes done by user 2 ");
 	}
 	
 	
