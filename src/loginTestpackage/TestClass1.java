8c8f43d1464ba58132e29d01cc6ab96a641f5018
RahulPerkiwar <rperkiwar2089@gmail.com>
2024-03-25 12:49:21 +0530

    task 1 of adding classess

---
+++ src/loginTestpackage/TestClass1.java
@@ -0,0 +1,11 @@
+package loginTestpackage;
+
+public class TestClass1 {
+
+	public void testClassMethod()
+{
+		
+     System.out.println("Class 1 is beaing addedd");
+
+}
+}
---
+++ src/loginTestpackage/TestClass2.java
@@ -0,0 +1,12 @@
+package loginTestpackage;
+
+public class TestClass2 {
+	
+	public void testClassMethod2()
+	{
+			
+	     System.out.println("Class 2 is beaing addedd");
+
+	}
+
+}
